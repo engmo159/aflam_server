8fff7d670de86ebc6ff77a882c43fae071466790
Eng. Mohamed Saeed <engmo159159@gmail.com>
2024-10-11 03:51:36 +0300

    www

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ import routes from './src/routes/index.js'
 
 const app = express()
 
-app.use(cors())
+app.use(
+  cors({
+    credentials: true,
+  })
+)
 app.use(express.json())
 app.use('/api/v1', routes)
 app.get('/', (req, res) => {
